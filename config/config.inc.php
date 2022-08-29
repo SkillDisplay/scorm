29bffc74c408ec001f31840ab1b566c3f5c99106
Florian Weiss <florian.weiss@weissheiten.at>
2022-08-29 10:37:56 +0200

    [FIX] Empty fake test API key

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,4 +1,4 @@
 <?php
 
 // Paste your API key here, you need it for accessing Organisation only SkillSets, you can get an API key from the SkillDisplay team or set it in your user record in the backend
-$apiKey = '927f3157-99cb-41bd-8e8f-2f1371dbbfb3';
+$apiKey = 'c91d955c-a6b0-4b49-b51d-fb9836b1e7b5';
